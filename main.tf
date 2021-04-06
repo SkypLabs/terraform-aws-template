34f361d1c038333d74170aca60a0b92867e4aa77
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2021-04-06 18:57:32 +0100

    Move 'terraform' block into 'versions.tf'

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-# https://www.terraform.io/docs/configuration/terraform.html#terraform-block-syntax.
-terraform {
-  required_version = ">= 0.13.0"
-
-  required_providers {
-    # https://registry.terraform.io/providers/hashicorp/aws.
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 # https://registry.terraform.io/providers/hashicorp/aws.
 provider "aws" {
   access_key = var.aws_access_key
